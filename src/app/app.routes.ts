3c020baf9f1112efcc002fd10637bbd01cc445e3
aleksandar-pejkovic <aleksandarpejkovic@hotmail.com>
2024-10-17 13:40:53 +0200

    add candidate create functionality2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CandidateListComponent } from './components/candidate-list/candidate-list.component';
+import { CandidateCreateComponent } from './components/candidate-create/candidate-create.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: 'candidates', component: CandidateListComponent },
+    { path: 'candidates/add', component: CandidateCreateComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
